457e275f33017760fb655a6d137c1e9bea124e9d
Akinbobola <71355010+phemmyblaze@users.noreply.github.com>
2025-06-20 10:54:24 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,42 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'img.clerk.com',
-                pathname: '**',
-            },
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        pathname: '**',
+      },
+    ],
+  },
+  async headers() {
+    const headers = [];
+    if (
+      process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview' ||
+      process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+    ) {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index',
+          },
+          {
+            key: 'Link',
+            value: ' < https://portfolio-codegiyu.vercel.app >; rel="canonical"',
+          },
         ],
-    },
+        source: '/:path*',
+      });
+    }
+    return headers;
+  },
 };
 
 export default nextConfig;
